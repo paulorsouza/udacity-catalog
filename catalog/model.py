3f4c5b6a239bca5406df2bf989ec14978d150bba
Paulo Ricardo <paulor1809@gmail.com>
2018-07-27 13:20:42 +0000

    Create pet type model

--- catalog/model.py
+++ catalog/model.py
@@ -24,6 +24,18 @@ class Family(Base):
     picture = Column(String)
     created_at = Column(DateTime, default=datetime.now)
 
+class Type(Base):
+    __tablename__ = 'pet_type'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    detail = Column(String)
+    created_at = Column(DateTime, default=datetime.now)
+    edited_at = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user_profile.id'))
+    user = relationship('User')
+    family_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = relationship(Category, back_populates='items')
+
 
 engine = create_engine('postgresql:///catalog')
  
